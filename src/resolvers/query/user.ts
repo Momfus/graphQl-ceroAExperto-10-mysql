2dd1804393ba066b2652f92deccbb4d6b1d24cb5
Momfus <momfus@outlook.com>
2020-12-21 01:16:46 -0300

    query, usuarios, definción y primeros pasos - clase 382

--- src/resolvers/query/user.ts
+++ src/resolvers/query/user.ts
@@ -2,6 +2,17 @@ import { IResolvers } from 'graphql-tools';
 
 const resolverQueryUsers: IResolvers = {
     Query: {
+        users() { // Devuelvo una lista de usuarios
+            return [
+                {
+                    id: 1,
+                    name: 'Fernando Herrera',
+                    instructor: 1,
+                    twitter: 'mugan86',
+                    web: ''
+                }
+            ];
+        },
         hello(): string {
             return 'Hello world!!';
         },
