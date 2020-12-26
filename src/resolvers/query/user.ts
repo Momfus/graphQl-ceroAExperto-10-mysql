7624f10322a1a1d45df96442aae31fcc18be305f
Momfus <momfus@outlook.com>
2020-12-26 01:32:00 -0300

    query - usuarios - obtener información - clase 383

--- src/resolvers/query/user.ts
+++ src/resolvers/query/user.ts
@@ -1,17 +1,34 @@
 import { IResolvers } from 'graphql-tools';
+import connection from '../config/db';
+import { USER_LIST } from '../../constants/db-operations';
+import { resolve } from 'dns';
 
 const resolverQueryUsers: IResolvers = {
     Query: {
-        users() { // Devuelvo una lista de usuarios
-            return [
-                {
-                    id: 1,
-                    name: 'Fernando Herrera',
-                    instructor: 1,
-                    twitter: 'mugan86',
-                    web: ''
-                }
-            ];
+        users(_, __, { connection }) { // Devuelvo una lista de usuarios
+            
+            const users = new Array(0);
+            
+            var sql = USER_LIST;
+            return new Promise( (resolve, reject) => {
+                connection.query(sql, function (error: any, results: any ) {
+
+                    if (error) { reject([]); } // Resultado incorrecto
+
+                    // Resultado correcto
+                    results.forEach( (element: any) => {
+                        users.push({
+                            id: element.id,
+                            name: element.name,
+                            instructor: element.instructor,
+                            twitter: element.twitter,
+                            web: element.web
+                        });
+                    });
+                    resolve(users);
+
+                });
+            }); 
         },
         hello(): string {
             return 'Hello world!!';
