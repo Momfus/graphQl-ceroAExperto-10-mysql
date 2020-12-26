c318978dbd57aa0cfc570a57bd00e3739e7b0064
Momfus <momfus@outlook.com>
2020-12-26 01:55:44 -0300

    query - usuario - obtener información del item seleccionado - clase 384

--- src/constants/db-operations.ts
+++ src/constants/db-operations.ts
@@ -1,1 +1,2 @@
 export const USER_LIST = 'SELECT * FROM `users`';
+export const USER_SELECT_DETAILS = 'SELECT * FROM `users` WHERE id = ?';
--- src/resolvers/query/user.ts
+++ src/resolvers/query/user.ts
@@ -1,45 +1,76 @@
 import { IResolvers } from 'graphql-tools';
 import connection from '../config/db';
-import { USER_LIST } from '../../constants/db-operations';
+import { USER_LIST, USER_SELECT_DETAILS } from '../../constants/db-operations';
 import { resolve } from 'dns';
 
 const resolverQueryUsers: IResolvers = {
-    Query: {
-        users(_, __, { connection }) { // Devuelvo una lista de usuarios
-            
-            const users = new Array(0);
-            
-            var sql = USER_LIST;
-            return new Promise( (resolve, reject) => {
-                connection.query(sql, function (error: any, results: any ) {
+  Query: {
+    users(_, __, { connection }) {
+      // Devuelvo una lista de usuarios
 
-                    if (error) { reject([]); } // Resultado incorrecto
+      const users = new Array(0);
 
-                    // Resultado correcto
-                    results.forEach( (element: any) => {
-                        users.push({
-                            id: element.id,
-                            name: element.name,
-                            instructor: element.instructor,
-                            twitter: element.twitter,
-                            web: element.web
-                        });
-                    });
-                    resolve(users);
+      const sql = USER_LIST;
+      return new Promise((resolve, reject) => {
+        connection.query(sql, function (error: any, results: any) {
+          if (error) {
+            reject([]);
+          } // Resultado incorrecto
 
-                });
-            }); 
-        },
-        hello(): string {
-            return 'Hello world!!';
-        },
-        helloWithName(_: void, args): string {
-            return `Hello ${args.name}!!`;
-        },
-        helloToGraphQLCourse(): string {
-            return 'Hello to GraphQL Course!!';
-        }
-    }
+          // Resultado correcto
+          results.forEach((element: any) => {
+            users.push({
+              id: element.id,
+              name: element.name,
+              instructor: element.instructor,
+              twitter: element.twitter,
+              web: element.web,
+            });
+          });
+          resolve(users);
+        });
+      });
+    },
+    user(_, { id }, { connection }) {
+      // Devuelvo un usuario por id
+
+      const sql = USER_SELECT_DETAILS;
+      return new Promise((resolve, reject) => {
+        connection.query(sql, [id], function (error: any, results: any) {
+          if (error) {
+            reject(null);
+          } // Resultado incorrecto
+
+          // Resultado correcto
+          const element = results[0];
+          let user;
+
+          if (element === undefined || element === null) {
+            user = null;
+          }else {
+            user = {
+              id: element.id,
+              name: element.name,
+              instructor: element.instructor,
+              twitter: element.twitter,
+              web: element.web,
+            };
+          }
+          resolve( user);
+
+        });
+      });
+    },
+    hello(): string {
+      return 'Hello world!!';
+    },
+    helloWithName(_: void, args): string {
+      return `Hello ${args.name}!!`;
+    },
+    helloToGraphQLCourse(): string {
+      return 'Hello to GraphQL Course!!';
+    },
+  },
 };
 
 export default resolverQueryUsers;
